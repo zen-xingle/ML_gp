e71e9f6ebabca52212c7a7a6da801266d3c6b69b
zen-xingle <739934831@qq.com>
2022-05-18 00:02:53 +0800

    [non-subset demo init]

---
+++ demo/non-subset/demo_dc_cigp.py
@@ -0,0 +1,134 @@
+import os
+import sys
+import torch
+
+realpath=os.path.abspath(__file__)
+_sep = os.path.sep
+realpath = realpath.split(_sep)
+realpath = _sep.join(realpath[:realpath.index('ML_gp')+1])
+sys.path.append(realpath)
+
+from utils.main_controller import controller
+from module.cigp import CIGP_MODULE
+from module.dc_cigp import DC_CIGP_MODULE
+
+real_dataset = ['FlowMix3D_MF',
+                'MolecularDynamic_MF', 
+                'plasmonic2_MF', 
+                'SOFC_MF',]
+
+gen_dataset = ['poisson_v4_02',
+                'burger_v4_02',
+                'Burget_mfGent_v5',
+                'Burget_mfGent_v5_02',
+                # 'Heat_mfGent_v5',
+                'Piosson_mfGent_v5',
+                'Schroed2D_mfGent_v1',
+                'TopOP_mfGent_v5',]
+interp_data=True
+
+
+if __name__ == '__main__':
+    # for _dataset in real_dataset + gen_dataset:
+    for _dataset in ['SOFC_MF']:
+        for _seed in [None, 0, 1, 2, 3, 4]:
+            with open('record.txt', 'a') as _temp_file:
+                _temp_file.write('-'*40 + '\n')
+                _temp_file.write('\n')
+                _temp_file.write('  Demo NAR cigp \n')
+                _temp_file.write('  seed: {} \n'.format(_seed))
+                _temp_file.write('  interp_data: {} \n'.format(interp_data))
+                _temp_file.write('\n')
+                _temp_file.write('-'*40 + '\n')
+                _temp_file.write('-'*3 + '> Training x -> yl part\n')
+                _temp_file.flush()
+
+            # ================================================================
+            # Training x -> yl part
+
+            controller_config = {'max_epoch':1000} # use defualt config
+            module_config = {
+                'dataset': {'name': _dataset,
+                            'interp_data': interp_data,
+
+                            'seed': _seed,
+                            'train_start_index': 0, 
+                            'train_sample': 32, 
+                            'eval_start_index': 0,
+                            'eval_sample': 128,
+
+                            'inputs_format': ['x[0]'],
+                            'outputs_format': ['y[0]'],
+
+                            'force_2d': True,
+                            'x_sample_to_last_dim': False,
+                            'y_sample_to_last_dim': False,
+                            'slice_param': [0.6, 0.4], #only available for dataset, which not seperate train and test before
+                            },
+            } # only change dataset config, others use default config
+            ct = controller(CIGP_MODULE, controller_config, module_config)
+            ct.start_train()
+            ct.smart_restore_state(-1)
+            ct.rc_file.write('---> final result\n')
+            ct.rc_file.flush()
+            ct.start_eval({'eval state':'final'})
+            ct.rc_file.write('-'*10 + '> finish x-yl training\n\n')
+            ct.rc_file.flush()
+
+
+            # ================================================================
+            # Training x,yl -> yh part
+            for _sample in [4,8,16,32]:
+                with open('record.txt', 'a') as _temp_file:
+                    _temp_file.write('\n'+ '-'*10 + '>\n')
+                    _temp_file.write('NAR for {} samples\n'.format(_sample))
+                    _temp_file.write('-'*3 + '> Training x,yl -> yh part\n\n')
+                    _temp_file.flush()
+
+                second_controller_config = {
+                    'max_epoch': 1000,
+                }
+                second_module_config = {
+                    'dataset': {'name': _dataset,
+                                'interp_data': interp_data,
+
+                                'seed': _seed,
+                                'train_start_index': 0, 
+                                'train_sample': _sample, 
+                                'eval_start_index': 0,
+                                'eval_sample': 128,
+
+                                'inputs_format': ['x[0]','y[0]'],
+                                'outputs_format': ['y[-1]'],
+
+                                'force_2d': True,
+                                'x_sample_to_last_dim': False,
+                                'y_sample_to_last_dim': False,
+                                'slice_param': [0.6, 0.4], #only available for dataset, which not seperate train and test before
+                                },
+                    'pca': {'type': 'listPCA', 
+                            'r': 0.99, }, # listPCA, resPCA_mf,
+                    'noise_init' : 1000.,
+                }
+                second_ct = controller(DC_CIGP_MODULE, controller_config, second_module_config)
+                # replace ground truth eval data with low fidelity predict
+                # check inputs x
+                x_dim = ct.module.inputs_eval[0].shape[1]
+                torch.dist(second_ct.module.inputs_eval[0][:,0:x_dim], ct.module.inputs_eval[0])
+                # check inputs y
+                torch.dist(second_ct.module.inputs_eval[1], ct.module.outputs_eval[0])
+                # check predict y
+                torch.dist(second_ct.module.inputs_eval[1], ct.module.predict_y)
+                second_ct.module.inputs_eval[1] = ct.module.predict_y
+
+                second_ct.start_train()
+                second_ct.smart_restore_state(-1)
+                second_ct.rc_file.write('---> final result\n')
+                second_ct.rc_file.flush()
+                second_ct.start_eval({'eval state':'final',
+                        'module_name':'DC_cigp',
+                        'cp_record_file': True})
+                second_ct.rc_file.write('---> end\n\n')
+                second_ct.rc_file.flush()
+
+    second_ct.clear_record()
---
+++ demo/non-subset/demo_nar_cigp.py
@@ -0,0 +1,166 @@
+import os
+import sys
+import torch
+
+realpath=os.path.abspath(__file__)
+_sep = os.path.sep
+realpath = realpath.split(_sep)
+realpath = _sep.join(realpath[:realpath.index('ML_gp')+1])
+sys.path.append(realpath)
+
+from utils.main_controller import controller
+from module.cigp import CIGP_MODULE
+
+interp_data=False
+
+real_dataset = ['FlowMix3D_MF',
+                'MolecularDynamic_MF', 
+                'plasmonic2_MF', 
+                'SOFC_MF',]
+
+gen_dataset = ['poisson_v4_02',
+                'burger_v4_02',
+                'Burget_mfGent_v5',
+                'Burget_mfGent_v5_02',
+                # 'Heat_mfGent_v5',
+                'Piosson_mfGent_v5',
+                'Schroed2D_mfGent_v1',
+                'TopOP_mfGent_v5',]
+
+
+def non_subset(first_module, second_module):
+    from copy import deepcopy
+    # torch.dist(first_module.inputs_tr[0], second_module.inputs_tr[0][:,0:first_module.inputs_tr[0].shape[1]])
+    f_start_index = first_module.module_config['dataset']['train_start_index']
+    f_sample = first_module.module_config['dataset']['train_sample']
+    f_input = deepcopy(first_module.inputs_tr[0])
+    if first_module.module_config['input_normalize'] is True:
+        f_input = first_module.X_normalizer.denormalize(f_input)
+    s_start_index = second_module.module_config['dataset']['train_start_index']
+    s_sample = second_module.module_config['dataset']['train_sample']
+    # assert s_sample == f_sample
+
+    subset_number = max(f_start_index + f_sample - s_start_index, 0)
+    subset_number = min(subset_number, s_sample)
+
+    subset_start_index = s_start_index
+    s_input = deepcopy(second_module.inputs_tr[0])
+    s_subset_input = s_input[:subset_number,...]
+    if second_module.module_config['input_normalize'] is True:
+        s_subset_input = second_module.X_normalizer.denormalize(s_subset_input)
+    # torch.dist(f_input[subset_start_index:,...], s_subset_input[:, :f_input.shape[-1]]) # -> 0
+    # update non-subset
+    non_subset_input = s_input[subset_number:, :f_input.shape[-1]]
+    predict_u, _ = first_module.predict([non_subset_input])
+    new_input = torch.cat([non_subset_input, predict_u], dim=1)
+    new_input = torch.cat([s_subset_input, new_input], dim=0)
+    second_module.inputs_tr[0] = deepcopy(new_input)
+    if second_module.module_config['input_normalize'] is True:
+        second_module.inputs_tr[0] = second_module.X_normalizer.normalize(second_module.inputs_tr[0])
+
+
+if __name__ == '__main__':
+    # for _dataset in real_dataset + gen_dataset:
+    for _dataset in ['SOFC_MF']:
+        for _seed in [None, 0, 1, 2, 3, 4]:
+            with open('record.txt', 'a') as _temp_file:
+                _temp_file.write('-'*40 + '\n')
+                _temp_file.write('\n')
+                _temp_file.write('  Demo NAR cigp \n')
+                _temp_file.write('  seed: {} \n'.format(_seed))
+                _temp_file.write('  interp_data: {} \n'.format(interp_data))
+                _temp_file.write('\n')
+                _temp_file.write('-'*40 + '\n')
+                _temp_file.write('-'*3 + '> Training x -> yl part\n')
+                _temp_file.flush()
+
+            # ================================================================
+            # Training x -> yl part
+
+            controller_config = {} # use defualt config
+            module_config = {
+                'dataset': {'name': _dataset,
+                            'interp_data': interp_data,
+
+                            'seed': _seed,
+                            'train_start_index': 0, 
+                            'train_sample': 32, 
+                            'eval_start_index': 0,
+                            'eval_sample': 128,
+
+                            'inputs_format': ['x[0]'],
+                            'outputs_format': ['y[0]'],
+
+                            'force_2d': True,
+                            'x_sample_to_last_dim': False,
+                            'y_sample_to_last_dim': False,
+                            'slice_param': [0.6, 0.4], #only available for dataset, which not seperate train and test before
+                            },
+            } # only change dataset config, others use default config
+            ct = controller(CIGP_MODULE, controller_config, module_config)
+            ct.start_train()
+            ct.smart_restore_state(-1)
+            ct.rc_file.write('---> final result\n')
+            ct.rc_file.flush()
+            ct.start_eval({'eval state':'final'})
+            ct.rc_file.write('-'*10 + '> finish x-yl training\n\n')
+            ct.rc_file.flush()
+
+
+            # ================================================================
+            # Training x,yl -> yh part
+            total_sample = 32
+            for start_index in [total_sample-4, total_sample-8, total_sample-16, total_sample-32]:
+                with open('record.txt', 'a') as _temp_file:
+                    _temp_file.write('\n'+ '-'*10 + '>\n')
+                    _temp_file.write('NAR for {} subset samples\n'.format(total_sample - start_index))
+                    _temp_file.write('-'*3 + '> Training x,yl -> yh part\n\n')
+                    _temp_file.flush()
+
+                second_controller_config = {
+                    'max_epoch': 1000,
+                }
+                second_module_config = {
+                    'dataset': {'name': _dataset,
+                                'interp_data': interp_data,
+
+                                'seed': _seed,
+                                'train_start_index': start_index, 
+                                'train_sample': total_sample, 
+                                'eval_start_index': 0,
+                                'eval_sample': 128,
+
+                                'inputs_format': ['np.concatenate([x[0],y[0]], axis=1)'],
+                                'outputs_format': ['y[-1]'],
+
+                                'force_2d': True,
+                                'x_sample_to_last_dim': False,
+                                'y_sample_to_last_dim': False,
+                                'slice_param': [0.6, 0.4], #only available for dataset, which not seperate train and test before
+                                },
+                }
+                second_ct = controller(CIGP_MODULE, controller_config, second_module_config)
+                # replace ground truth eval data with low fidelity predict
+                # check inputs x
+                x_dim = ct.module.inputs_eval[0].shape[1]
+                torch.dist(second_ct.module.inputs_eval[0][:,0:x_dim], ct.module.inputs_eval[0])
+                # check inputs y
+                torch.dist(second_ct.module.inputs_eval[0][:,x_dim:], ct.module.outputs_eval[0])
+                # check predict y
+                torch.dist(second_ct.module.inputs_eval[0][:,x_dim:], ct.module.predict_y)
+                second_ct.module.inputs_eval[0] = torch.cat([ct.module.inputs_eval[0], ct.module.predict_y],dim=1)
+                non_subset(ct.module, second_ct.module)
+
+                # continue
+                second_ct.start_train()
+                second_ct.smart_restore_state(-1)
+                second_ct.rc_file.write('---> final result\n')
+                second_ct.rc_file.flush()
+                second_ct.start_eval({'eval state':'final',
+                                    'module_name':'NAR',
+                                    'subset': str(total_sample - start_index),
+                                    'cp_record_file': True})
+                second_ct.rc_file.write('---> end\n\n')
+                second_ct.rc_file.flush()
+
+    second_ct.clear_record()

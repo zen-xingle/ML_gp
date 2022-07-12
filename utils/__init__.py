d33774e37fb77dd39467b2da691eef41b165e686
zen-xingle <739934831@qq.com>
2022-07-13 00:50:43 +0800

    [new feature add] add py_decorator file. now support check class init param.

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,3 +5,5 @@ from utils.data_loader import SP_DataLoader, Standard_mat_DataLoader
 from utils.data_preprocess import Data_preprocess
 from utils.ResPCA import listPCA, resPCA_mf
 from utils.dict_tools import smart_update
+from utils.mlgp_log import mlgp_log
+from utils.mglp_hook import register_nan_hook

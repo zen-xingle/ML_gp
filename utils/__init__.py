be739c686cf0fcf9d26ca35dc71b1ea9d194c9d7
zen-xingle <739934831@qq.com>
2022-07-16 19:01:34 +0800

    [new feature] add data register function

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,11 @@
 from utils.eigen import eigen_pairs
 from utils.normalizer import Normalizer
 from utils.performance_evaluator import performance_evaluator, high_level_evaluator
-from utils.data_loader import SP_DataLoader, Standard_mat_DataLoader
-from utils.data_preprocess import Data_preprocess
 from utils.ResPCA import listPCA, resPCA_mf
 from utils.dict_tools import smart_update
 from utils.mlgp_log import mlgp_log
 from utils.mglp_hook import register_nan_hook
+
+JITTER = 1e-6
+EPS = 1e-10
+PI = 3.1415

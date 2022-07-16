8e849cb327b03bb0f712555b871b54c4be9e53ec
zen-xingle <739934831@qq.com>
2022-07-16 19:02:52 +0800

    [code optimize] dict tools using mlgp_log to notice what has change.

--- utils/dict_tools.py
+++ utils/dict_tools.py
@@ -1,8 +1,16 @@
 from copy import deepcopy
+from utils.mlgp_log import mlgp_log
 
 def smart_update(dst, src):
+    # 目前最多支持两级dict
     dst = deepcopy(dst)
     src = deepcopy(src)
+
+    mlgp_log.i("\nInput config as follow:")
+    for _key, value in src.items():
+        mlgp_log.i(_key, ':', '{}'.format(value))
+    mlgp_log.i("\n")
+
     _used_keys = []
     for _key, value in src.items():
         if isinstance(value, dict):
@@ -13,4 +21,10 @@ def smart_update(dst, src):
     for _key in _used_keys:
         src.pop(_key)
     dst.update(src)
+
+    mlgp_log.i("\nInput config, after fullfill, meaning what actually feed to the model, as follow:")
+    for _key, value in dst.items():
+        mlgp_log.i(_key, ':', '{}'.format(value))
+    mlgp_log.i("\n")
+
     return dst

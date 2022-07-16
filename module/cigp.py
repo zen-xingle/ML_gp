3a3bdd28e1b60b0e6ad7ae97699125e143183790
zen-xingle <739934831@qq.com>
2022-07-16 19:05:02 +0800

    [cigp] optimize code

--- module/cigp.py
+++ module/cigp.py
@@ -8,7 +8,6 @@ import sys
 import random
 
 from copy import deepcopy
-from scipy.io import loadmat
 
 realpath=os.path.abspath(__file__)
 _sep = os.path.sep
@@ -17,10 +16,8 @@ realpath = _sep.join(realpath[:realpath.index('ML_gp')+1])
 sys.path.append(realpath)
 
 from utils import *
-
-JITTER = 1e-6
-EPS = 1e-10
-PI = 3.1415
+from kernel import kernel_utils
+from utils.data_utils import data_register
 
 tensorly.set_backend('pytorch')
 
@@ -60,9 +57,10 @@ default_module_config = {
 }
 
 
-class CIGP_MODULE:
+class CIGP_MODULE(torch.nn.Module):
     # def __init__(self, grid_params_list, kernel_list, target_list, normalize=True, restrict_method= 'exp') -> None:
     def __init__(self, module_config) -> None:
+        super().__init__()
         _final_config = smart_update(default_module_config, module_config)
         self.module_config = deepcopy(_final_config)
         module_config = deepcopy(_final_config)
@@ -71,7 +69,12 @@ class CIGP_MODULE:
         assert module_config['optimizer'] in ['adam'], 'now optimizer only support adam, but get {}'.format(module_config['optimizer'])
 
         # load_data
-        self._load_data(module_config['dataset'])
+        data_register.data_regist(self, module_config['dataset'])
+        # self.inputs_tr[0] = self.inputs_tr[0].cuda()
+        # self.outputs_tr[0] = self.outputs_tr[0].cuda()
+
+        self.inputs_tr[0] = self.inputs_tr[0]
+        self.outputs_tr[0] = self.outputs_tr[0]
 
         # X - normalize
         if module_config['input_normalize'] is True:
@@ -88,7 +91,7 @@ class CIGP_MODULE:
             self.Y_normalizer = None
 
         # init kernel
-        self._init_kernel(module_config['kernel'])
+        kernel_utils.register_kernel(self, module_config['kernel'])
 
         # init noise
         if module_config['exp_restrict'] is True:
@@ -98,55 +101,6 @@ class CIGP_MODULE:
 
         # init optimizer
         self._optimizer_setup()
-
-    def _load_data(self, dataset_config):
-        print('dataset_config name:', dataset_config['name'])
-        loaded = False
-        for _loader in [SP_DataLoader, Standard_mat_DataLoader]:
-            if dataset_config['name'] in _loader.dataset_available:
-                self.data_loader = _loader(dataset_config['name'], dataset_config['interp_data'])
-                _data = self.data_loader.get_data()
-                loaded = True
-                break
-        if loaded is False:
-            assert False, 'dataset {} not found in all loader'.format(dataset_config['name'])
-
-        dp = Data_preprocess(dataset_config)
-        self.inputs_tr, self.outputs_tr, self.inputs_eval, self.outputs_eval = dp.do_preprocess(_data, numpy_to_tensor=True)
-
-
-    def _select_connection_kernel(self, type_name):
-        from kernel.Multi_fidelity_connection import rho_connection, mapping_connection
-        assert type_name in ['res_standard', 'res_rho', 'res_mapping']
-        if type_name == 'res_standard':
-            self.target_connection = rho_connection(rho=1., trainable=False)
-        elif type_name in ['res_rho']:
-            self.target_connection = rho_connection(rho=1., trainable=True)
-        elif type_name in ['res_mapping']:
-            self.target_connection = mapping_connection(self.target_list[0][:,:,0].shape, 
-                                                        self.target_list[1][:,:,0].shape,
-                                                        self.module_config['mapping'])
-
-
-    def _init_kernel(self, kernel_config):
-        from kernel.kernel_generator import kernel_generator
-        self.kernel_list = []
-        for key, value in kernel_config.items():
-            for _kernel_type, _kernel_params in value.items():
-                # broadcast exp_restrict
-                if 'exp_restrict' not in _kernel_params:
-                    _kernel_params['exp_restrict'] = self.module_config['exp_restrict']
-                self.kernel_list.append(kernel_generator(_kernel_type, _kernel_params))
-
-    def get_params_need_check(self):
-        params_need_check = []
-        for i in range(len(self.kernel_list)):
-            params_need_check.extend(self.kernel_list[i].get_params_need_check())
-        params_need_check.append(self.noise)
-        
-        if hasattr(self, 'mapping_param'):
-            params_need_check.append(self.mapping_param)
-        return params_need_check
 
 
     def _optimizer_setup(self):
@@ -167,12 +121,12 @@ class CIGP_MODULE:
         # x: [num, vector_dims]
         # y: [num, vector_dims]
 
-        Sigma = self.kernel_list[0](self.inputs_tr[0], self.inputs_tr[0]) + JITTER * torch.eye(self.inputs_tr[0].size(0))
+        Sigma = self.kernel_list[0](self.inputs_tr[0], self.inputs_tr[0]) + JITTER * torch.eye(self.inputs_tr[0].size(0), device=self.inputs_tr[0].device)
         if self.module_config['exp_restrict'] is True:
             _noise = self.noise.exp()
         else:
             _noise = self.noise
-        Sigma = Sigma + _noise.pow(-1) * torch.eye(self.inputs_tr[0].size(0))
+        Sigma = Sigma + _noise.pow(-1) * torch.eye(self.inputs_tr[0].size(0), device=self.inputs_tr[0].device)
 
         L = torch.linalg.cholesky(Sigma)
         #option 1 (use this if torch supports)
@@ -183,7 +137,7 @@ class CIGP_MODULE:
 
         y_num, y_dimension = self.outputs_tr[0].shape
         nll =  0.5 * (gamma ** 2).sum() +  L.diag().log().sum() * y_dimension  \
-            + 0.5 * y_num * torch.log(2 * torch.tensor(PI)) * y_dimension
+            + 0.5 * y_num * torch.log(2 * torch.tensor(PI, device=self.inputs_tr[0].device)) * y_dimension
 
         return nll
 
@@ -196,12 +150,12 @@ class CIGP_MODULE:
             if self.module_config['input_normalize'] is True:
                 input_param[0] = self.X_normalizer.normalize(input_param[0])
 
-            Sigma = self.kernel_list[0](self.inputs_tr[0], self.inputs_tr[0]) + JITTER * torch.eye(self.inputs_tr[0].size(0))
+            Sigma = self.kernel_list[0](self.inputs_tr[0], self.inputs_tr[0]) + JITTER * torch.eye(self.inputs_tr[0].size(0), device=self.inputs_tr[0].device)
             if self.module_config['exp_restrict'] is True:
                 _noise = self.noise.exp()
             else:
                 _noise = self.noise
-            Sigma = Sigma + _noise.pow(-1) * torch.eye(self.inputs_tr[0].size(0))
+            Sigma = Sigma + _noise.pow(-1) * torch.eye(self.inputs_tr[0].size(0), device=self.inputs_tr[0].device)
 
             kx = self.kernel_list[0](self.inputs_tr[0], input_param[0])
             L = torch.cholesky(Sigma)
@@ -224,26 +178,6 @@ class CIGP_MODULE:
         self.optimizer.step()
         # print('loss_nll:', loss.item())
 
-
-    def save_state(self):
-        state_dict = []
-        for i, _kernel in enumerate(self.kernel_list):
-            state_dict.extend(_kernel.get_param([]))
-
-        state_dict.append(self.noise)
-        return state_dict
-
-
-    def load_state(self, params_list):
-        index = 0
-        for i, _kernel in enumerate(self.kernel_list):
-            _temp_list = _kernel.get_param([])
-            _kernel.set_param(params_list[index: index + len(_temp_list)])
-            index += len(_temp_list)
-        
-        with torch.no_grad():
-            self.noise.copy_(params_list[index])
-            index += 1
 
     def eval(self):
         print('---> start eval')

5dac3158f173d0629b9c169da92a6f3df7d38caf
zen-xingle <739934831@qq.com>
2022-07-17 18:55:28 +0800

    [data register support cuda]

--- utils/data_utils/data_register.py
+++ utils/data_utils/data_register.py
@@ -13,7 +13,7 @@ from utils.data_utils.data_preprocess import Data_preprocess
 from utils.data_utils.data_loader import SP_DataLoader, Standard_mat_DataLoader
 
 
-def data_regist(module_calsee, dataset_config):
+def data_regist(module_calsee, dataset_config, cuda=False):
     mlgp_log.i('dataset_config name:', dataset_config['name'])
     loaded = False
     for _loader in [SP_DataLoader, Standard_mat_DataLoader]:
@@ -27,3 +27,8 @@ def data_regist(module_calsee, dataset_config):
 
     dp = Data_preprocess(dataset_config)
     module_calsee.inputs_tr, module_calsee.outputs_tr, module_calsee.inputs_eval, module_calsee.outputs_eval = dp.do_preprocess(_data, numpy_to_tensor=True)
+    
+    if cuda is True:
+        for _params in [module_calsee.inputs_tr, module_calsee.outputs_tr, module_calsee.inputs_eval, module_calsee.outputs_eval]:
+            for i, _p in enumerate(_params):
+                _params[i] = _p.cuda()

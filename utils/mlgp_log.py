274f4f08e6d3f1b189e0ea5cfa1f33238c5c13ff
zen-xingle <739934831@qq.com>
2022-07-17 17:47:47 +0800

    [mlgo log] add example print

--- utils/mlgp_log.py
+++ utils/mlgp_log.py
@@ -40,3 +40,10 @@ class mlgp_log:
     def d(*args):
         meg = ' '.join(args)
         print('\033[1;45m' + 'DEBUG: ' + meg + '\033[0m')
+
+
+if __name__ == '__main__':
+    mlgp_log.i('This is info')
+    mlgp_log.w('This is warning')
+    mlgp_log.e('This is error')
+    mlgp_log.d('This is debug')

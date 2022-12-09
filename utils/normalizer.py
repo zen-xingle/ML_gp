7b5b0a53180a53379f24032a46d67097425240d3
zen-xingle <739934831@qq.com>
2022-12-09 23:41:16 +0800

    [normalizer] remove dump

--- utils/normalizer.py
+++ utils/normalizer.py
@@ -11,14 +11,7 @@ class Normalizer:
     #         self.mean = inputs.mean(dim=dim)
     #         self.std = inputs.std(dim=dim)
 
-    # def normalize(self, inputs):
-    #     # it should be auto broadcast
-    #     return (inputs - self.mean) / (self.std + 1e-8)
-
-    # def denormalize(self, inputs):
-    #     # it should be auto broadcast
-    #     return inputs * self.std + self.mean
-            # default dim is 0, which means the first dim is sample_num dim.
+        # default dim is 0, which means the first dim is sample_num dim.
         self.mean = inputs.mean(dim=dim, keepdim=True)
         self.std = inputs.std(dim=dim, keepdim=True)
         self.dim = dim
